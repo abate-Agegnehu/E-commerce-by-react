fe56834d49ff5e06c808acf0942264ee1b3208fe
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2024-10-18 01:36:08 +0300

    nav bar for ecommerce website
    nav bar for ecommerce website

---
+++ ecommerce/src/components/navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+import logo from "../assets/logo.png";
+import cart_icon from "../assets/cart_icon.png";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+  return (
+    <div className="navbar">
+      <div className="nav_logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav_menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}{" "}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link to="/mens" style={{ textDecoration: "none" }}>
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link to="/womens" style={{ textDecoration: "none" }}>
+            Women
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link to="/kids" style={{ textDecoration: "none" }}>
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav_login_cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="cart_count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
